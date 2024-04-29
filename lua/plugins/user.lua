0e99c1cc75f5d7ecdd609df4c3209700bf8c73d5
Yusuf Emirhan Şahin <syusufemirhan@gmail.com>
2024-04-29 15:06:18 +0300

    origin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
@@ -82,4 +81,94 @@ return {
       )
     end,
   },
+-- Copilot
+    {
+        'projekt0n/github-nvim-theme',
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+            },
+    {
+        'Shatur/neovim-ayu',
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+
+        end,
+    }, {
+    'olivercederborg/poimandres.nvim'
+    ,
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+
+    end,
+},
+    {
+        "zbirenbaum/copilot.lua",
+        enabled = true,
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                panel = {
+                    enabled = true,
+                    auto_refresh = true,
+                    keymap = {
+                        jump_next = "<c-j>",
+                        accept = "<c-k>",
+                        refresh = "r",
+                        open = "<M-CR>",
+                    },
+                    layout = {
+                        position = "bottom", -- | top | left | right
+                        ratio = 0.4,
+                    },
+                },
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    debounce = 75,
+                    keymap = {
+                        accept = "<c-a>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<c-j>",
+                        prev = "<c-k>",
+                        dismiss = "<C-e>",
+                    },
+                },
+            })
+        end
+    },
+    -- copilot cmp
+    {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+            require("copilot_cmp").setup()
+        end
+    },
+    {
+        'folke/tokyonight.nvim'
+    },
+    {
+        "fatih/vim-go",
+    },
+    {
+        "Shatur/neovim-ayu"
+    },
+    { "catppuccin/nvim",      name = "catppuccin",
+config = function()
+            
+
+            vim.cmd('colorscheme catppuccin-frappe')
+        end,
+
+  },
+    { 'jwalton512/vim-blade', lazy = true,        ft = "php" },
+    {
+        'fatih/vim-go',
+        lazy = true,
+        build = ":GoInstallBinaries",
+        ft = "go",
+    },
 }
